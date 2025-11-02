e4b321534dc526ec2fa8952a16d1de21c91dc418
richard-shepherd <richard.s.shepherd@gmail.com>
2025-11-02 19:52:53 +0000

    Messaging Mesh

---
+++ MessagingMeshLib/_PostBuild.cmd
@@ -0,0 +1,9 @@
+rem We copy header files needed by clients...
+SET header_folder="..\build-output\MessagingMeshLib\headers"
+mkdir %header_folder%
+copy MessagingMesh.h %header_folder%
+copy Connection.h %header_folder%
+copy Field.h %header_folder%
+copy Logger.h %header_folder%
+copy Message.h %header_folder%
+
