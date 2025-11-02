1584cc94afbb615d69b85b044568eee51a36e24c
richard-shepherd <richard.s.shepherd@gmail.com>
2025-11-02 19:40:21 +0000

    Messaging Mesh

---
+++ MessagingMeshLib/MessagingMesh.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Header file to include for Messaging Mesh clients.
+// Includes all the headers needed for client code.
+#include "Connection.h"
+#include "Field.h"
+#include "Logger.h"
+#include "Message.h"
