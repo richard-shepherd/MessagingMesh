ef4e6853742d3bca069c0ad7c0b40def85bce608
richard-shepherd <richard.s.shepherd@gmail.com>
2025-11-13 21:52:35 +0000

    Messaging Mesh

--- MessagingMeshLib.NET/Field.cs
+++ MessagingMeshLib.NET/Field.cs
@@ -43,6 +43,7 @@ namespace MessagingMeshLib.NET
         /// </summary>
         void serialize(Buffer buffer)
         {
+            // RSSTODO: WRITE THIS!!!
             throw new NotImplementedException();
         }
 
@@ -51,7 +52,68 @@ namespace MessagingMeshLib.NET
         /// </summary>
         void deserialize(Buffer buffer)
         {
+            // RSSTODO: WRITE THIS!!!
             throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Getters and setters for field types
+
+        /// <summary>
+        /// Gets the string held by the field.
+        /// Throws a MessagingMeshException if the field does not hold this type.
+        /// </summary>
+        public string getString()
+        {
+            checkDataType(DataType.STRING);
+            return m_data as string;
+        }
+
+        /// <summary>
+        /// Sets the field to hold a string.
+        /// </summary>
+        void setString(string value)
+        {
+            m_dataType = DataType.STRING;
+            m_data = value;
+        }
+
+        //// Gets the signed int32 held by the field.
+        //// Throws a MessagingMesh::Exception if the field does not hold this type.
+        //int32_t getSignedInt32() const;
+
+        //// Sets the field to hold a signed int32.
+        //void setSignedInt32(int32_t value);
+
+        //// Gets the double held by the field.
+        //// Throws a MessagingMesh::Exception  if the field does not hold this type.
+        //double getDouble() const;
+
+        //// Sets the field to hold a double.
+        //void setDouble(double value);
+
+        //// Gets the message held by the field.
+        //// Throws a MessagingMesh::Exception if the field does not hold this type.
+        //const ConstMessagePtr& getMessage() const;
+
+        //// Sets the field to hold a message.
+        //void setMessage(const ConstMessagePtr& value);
+
+        #endregion
+
+        #region Private functions
+
+        /// <summary>
+        /// Checks that the data being held is the type specified.
+        /// Throws a MessagingMeshException if it is not.
+        /// </summary>
+        private void checkDataType(DataType dataType)
+        {
+            if (m_dataType != DataType.STRING)
+            {
+                throw new MessagingMeshException($"Field {m_name} is not a {dataType}");
+            }
         }
 
         #endregion
---
+++ MessagingMeshLib.NET/MessagingMeshException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MessagingMeshLib.NET
+{
+    /// <summary>
+    /// Exception thrown by messaging mesh code.
+    /// </summary>
+    public class MessagingMeshException : Exception
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MessagingMeshException(string message)
+            : base(message)
+        {
+        }
+
+        #endregion
+    }
+}
