e48c6bb2ce3bb875f6fc0804683d4122464b3e9f
richard-shepherd <richard.s.shepherd@gmail.com>
2025-12-02 19:13:07 +0000

    Implementing the mesh

--- Gateway/_PostBuild.cmd
+++ Gateway/_PostBuild.cmd
@@ -2,5 +2,5 @@ set SOLUTION_DIR=%1
 set OUT_DIR=%2
 
 rem We copy mesh-config to the output folder...
-copy "%SOLUTION_DIR%Solution Items\mesh-config.json" "%OUT_DIR%"
+copy "%SOLUTION_DIR%Documents\mesh-config.json" "%OUT_DIR%"
 
