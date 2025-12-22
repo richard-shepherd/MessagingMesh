6d78cbe6f56ad4b93317544249afaeb1c3d20a6b
richard-shepherd <richard.s.shepherd@gmail.com>
2025-12-22 12:21:31 +0000

    Reporting message stats from the Gateway service managers

--- TestClient.NET/MixedLoadPublisher.cs
+++ TestClient.NET/MixedLoadPublisher.cs
@@ -1,5 +1,4 @@
-﻿using MessagingMeshLib.NET;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -26,24 +25,12 @@ namespace TestClient.NET
 
             // We create messages of various sizes and sore them in a 
             var messageInfos = new List<MessageInfo>();
-            messageInfos.Add(createMessageInfo("TINY-1", 10, 0, 0.1));
-            messageInfos.Add(createMessageInfo("TINY-2", 12, 0.1, 0.2));
-            messageInfos.Add(createMessageInfo("TINY-3", 13, 0.2, 0.3));
-            messageInfos.Add(createMessageInfo("SMALL-1", 100, 0.3, 0.4));
-            messageInfos.Add(createMessageInfo("SMALL-2", 120, 0.4, 0.5));
-            messageInfos.Add(createMessageInfo("SMALL-3", 130, 0.5, 0.6));
-            messageInfos.Add(createMessageInfo("MEDIUM-1", 1000, 0.6, 0.66));
-            messageInfos.Add(createMessageInfo("MEDIUM-2", 1000, 0.66, 0.72));
-            messageInfos.Add(createMessageInfo("MEDIUM-3", 1000, 0.72, 0.8));
-            messageInfos.Add(createMessageInfo("LARGE-1", 10000, 0.8, 0.83));
-            messageInfos.Add(createMessageInfo("LARGE-2", 10000, 0.83, 0.86));
-            messageInfos.Add(createMessageInfo("LARGE-3", 10000, 0.86, 0.9));
-            messageInfos.Add(createMessageInfo("VERY-LARGE-1", 100000, 0.9, 0.91));
-            messageInfos.Add(createMessageInfo("VERY-LARGE-2", 100000, 0.91, 0.93));
-            messageInfos.Add(createMessageInfo("VERY-LARGE-3", 100000, 0.93, 0.95));
-            messageInfos.Add(createMessageInfo("HUGE-1", 1000000, 0.95, 0.96));
-            messageInfos.Add(createMessageInfo("HUGE-2", 1000000, 0.96, 0.98));
-            messageInfos.Add(createMessageInfo("HUGE-3", 1000000, 0.98, 1.0));
+            createMessageInfos(messageInfos, "TINY",           10, 0.00, 0.30);
+            createMessageInfos(messageInfos, "SMALL",         100, 0.30, 0.60);
+            createMessageInfos(messageInfos, "MEDIUM",       1000, 0.60, 0.80);
+            createMessageInfos(messageInfos, "LARGE",       10000, 0.80, 0.90);
+            createMessageInfos(messageInfos, "VERY-LARGE", 100000, 0.90, 0.95);
+            createMessageInfos(messageInfos, "HUGE",      1000000, 0.95, 1.00);
 
             // We send updates...
             var rnd = new Random();
@@ -66,6 +53,25 @@ namespace TestClient.NET
                 // We sleep for a random time...
                 var intervalMS = rnd.Next(10, 100);
                 Thread.Sleep(intervalMS);
+            }
+        }
+
+        /// <summary>
+        /// Creates a number of message-infos and adds them to the collection.
+        /// </summary>
+        private static void createMessageInfos(List<MessageInfo> messageInfos, string baseSubject, int baseSize, double probabilityLowerBound, double probabilityUpperBound)
+        {
+            const int MESSAGES_TO_CREATE = 10;
+
+            var probabilityInterval = probabilityUpperBound - probabilityLowerBound;
+            var probabilityStep = probabilityInterval / MESSAGES_TO_CREATE;
+            for (var i = 0; i < MESSAGES_TO_CREATE; ++i)
+            {
+                var subject = $"{baseSubject}-{i}";
+                var size = baseSize + baseSize * i / MESSAGES_TO_CREATE;
+                var lowerBound = probabilityLowerBound + i * probabilityStep;
+                var upperBound = lowerBound + probabilityStep;
+                messageInfos.Add(createMessageInfo(subject, size, lowerBound, upperBound));
             }
         }
 
