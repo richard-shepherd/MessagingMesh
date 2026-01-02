786d731f581a016619da283e026ec7f774b92bc4
richard-shepherd <richard.s.shepherd@gmail.com>
2026-01-02 19:58:14 +0000

    Reporting message stats from the Gateway service managers

---
+++ Scripts/run_multiple_services.bat
@@ -0,0 +1,22 @@
+REM We start the gateway...
+pushd ..\build-output\Executables\Release
+start Gateway.exe --port 5050
+
+TIMEOUT 5
+
+REM We start publishing to various services...
+popd
+pushd ..\build-output\Executables\Release\TestClient.NET\net8.0
+start TestClient.NET.exe -pub-mixed -service=VULCAN
+start TestClient.NET.exe -pub-mixed -service=APOLLO
+start TestClient.NET.exe -pub-mixed -service=JUPITER
+start TestClient.NET.exe -pub-mixed -service=MERCURY
+start TestClient.NET.exe -pub-mixed -service=MINERVA
+start TestClient.NET.exe -pub-mixed -service=BACCHUS
+start TestClient.NET.exe -pub-mixed -service=VESTA
+
+REM We start the Coordinator
+popd
+pushd ..\build-output\Executables\Release\MessagingMeshCoordinator\net8.0
+start MessagingMeshCoordinator.exe
+
